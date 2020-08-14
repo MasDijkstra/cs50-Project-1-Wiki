7a732718ba35596de3ac7d50b6d245df8d009a8c
Mas Dijkstra <masdijkstra@masdijksmacbook.attlocal.net>
2020-08-13 23:50:53 -0400

    Implemented search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,9 @@
+from django import forms
 from django.shortcuts import render, redirect
 from django.urls import reverse
 import random, markdown2
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -17,7 +17,28 @@ def wiki(request, title):
             "content": markdown2.markdown(util.get_entry(title)),
         })
     else:
-        return render(request, "encyclopedia/error.html")
+        return 
+
+def search(request):
+    if request.method == "POST":
+        query = request.POST['q']
+
+        #return render(request, "encyclopedia/error.html")
+
+        if query.lower() in [entry.lower() for entry in util.list_entries()]:
+            return redirect("encyclopedia:wiki", title=query)
+
+        else:
+            matches = []
+            for entry in util.list_entries():
+                if query.lower() in entry.lower():
+                    matches.append(entry)
+                    
+            return render(request, "encyclopedia/searchresults.html", {
+                "query": query,
+                "matches": matches
+            })
+        
 
 def random_page(request):
     rand = random.choice(util.list_entries())
