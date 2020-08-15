afddaf4c1478f519758bc934908de5978d4ca8ac
Mas Dijkstra <masdijkstra@masdijksmacbook.attlocal.net>
2020-08-14 23:47:30 -0400

    Made the default CSS better

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,11 +5,15 @@ import random, markdown2
 
 from . import util
 
+#Index Page
+#Displays links to all entries
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+#Entry Page
+#Renders the contents of a wiki entry
 def wiki(request, title):
     if util.get_entry(title):
         return render(request, "encyclopedia/wiki.html", {
@@ -19,12 +23,12 @@ def wiki(request, title):
     else:
         return 
 
+#Search
+#Allows users to search for an entry. If none is found, display viable matches
 def search(request):
     if request.method == "POST":
         query = request.POST['q']
-
-        #return render(request, "encyclopedia/error.html")
-
+        
         if query.lower() in [entry.lower() for entry in util.list_entries()]:
             return redirect("encyclopedia:wiki", title=query)
 
@@ -38,8 +42,16 @@ def search(request):
                 "query": query,
                 "matches": matches
             })
-        
 
+
+#New Page
+#Allows users to create their own wiki entries
+#def new(request):
+#    return render(request, "encyclopedia:new")
+
+        
+#Random Page
+#Takes user to a random page
 def random_page(request):
     rand = random.choice(util.list_entries())
     return redirect("encyclopedia:wiki", title=rand)
