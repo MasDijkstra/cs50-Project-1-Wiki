a0609bcee68e0df3d39942024e62a5cfd9b140bd
Mas Dijkstra <masdijkstra@masdijksmacbook.attlocal.net>
2020-08-16 19:08:10 -0400

    Implemented Create New Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,14 @@
 from django import forms
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.urls import reverse
 import random, markdown2
 
 from . import util
+
+class CreatePageForm(forms.Form):
+    title = forms.CharField(max_length = 60)
+    content = forms.CharField(widget=forms.Textarea(attrs={"rows":10, "cols":10}))
 
 #Index Page
 #Displays links to all entries
@@ -27,7 +32,7 @@ def wiki(request, title):
 #Allows users to search for an entry. If none is found, display viable matches
 def search(request):
     if request.method == "POST":
-        query = request.POST['q']
+        query = request.POST["q"]
         
         if query.lower() in [entry.lower() for entry in util.list_entries()]:
             return redirect("encyclopedia:wiki", title=query)
@@ -43,13 +48,29 @@ def search(request):
                 "matches": matches
             })
 
-
-#New Page
+#Create Page
 #Allows users to create their own wiki entries
-#def new(request):
-#    return render(request, "encyclopedia:new")
+def create(request):
+    if request.method == "POST":
+        title, content = request.POST["title"], request.POST["content"]
 
-        
+        if title.lower() in [entry.lower() for entry in util.list_entries()]:
+            messages.error(request,"Error: A wiki entry with that name already exists.")
+            return redirect("encyclopedia:create")
+
+        else: 
+            util.save_entry(title, content)
+            return redirect("encyclopedia:wiki", title=title)
+
+    else:
+        return render(request, "encyclopedia/create.html")
+
+#Edit Page
+#Allows users to edit existing entries
+def edit(request):
+    pass
+
+
 #Random Page
 #Takes user to a random page
 def random_page(request):
